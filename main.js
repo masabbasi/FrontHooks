207d8755cb02b92bd57b684cf12459b0fceac482
MasAbbasi <masoud.abbasi.71@gmail.com>
2023-01-09 14:20:38 +0330

    All

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const hamburgerMenu = document.querySelector(".hamburger__menu");
 const menu = document.querySelector(".menu__right__items")
+const cover = document.querySelector(".cover")
+
 hamburgerMenu.addEventListener ("click",function(){
 	hamburgerMenu.classList.toggle("hamburger__menu--open");
 	menu.classList.toggle("mobile__menu--show")
+	cover.classList.toggle("cover--show")
 })
